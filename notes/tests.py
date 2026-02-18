2cb6791064dc15fdbb6fd987d6e88785e36d39a5
Pranisha4841 <pransamrzn@gmail.com>
2026-02-18 08:17:05 +0545

    first commit

---
+++ notes/tests.py
@@ -0,0 +1,20 @@
+from django.test import TestCase
+from .utils import add
+
+class AddTestCase(TestCase):
+    def test_add(self):
+        self.assertEqual(add(2, 3), 7)
+
+class NotesTestcase(TestCase):
+    def test_notes_can_be_added(self):
+        res=self.client.post(reverse('notes:add'),{
+            "title":"Hello",
+            "description":"Notes from test"
+        })
+
+        self.assertEqual(res.status_code,302)
+
+        reshome=self.client.get(res.url)
+
+        self.assertContains(reshome,"Hello")
+
